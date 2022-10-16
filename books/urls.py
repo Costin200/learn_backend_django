f23c9dca317c4e31fa3a91c769b38cfba18b8abc
Stefan-Eduard Deaconu <stefan@stefandeaconu.com>
2022-10-16 20:00:00 +0300

    Temporary requests

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    # path('', views.create, name='create'),
-    path('', views.read_all, name='read all'),
-    path('<int:index>', views.read, name='read'),
+    path('', views.index, name='create'),
+    path('<int:index>', views.one, name='read'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 import json
 
+import django.http.response
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -9,25 +11,83 @@ from django.shortcuts import render
 # CRUD   Create Read Update Delete
 
 books = [
-    "To Kill a Mockingbird", "Lord of The Rings"
+    "To Kill a Mockingbird",
+    "Lord of The Rings"
 ]
 
-# Create
-from django.views.decorators.csrf import csrf_protect
+# Pentru request-ul care foloseste metoda POST, este nevoie sa descriem cum
+#  arata formularul:
+
+from django import forms
 
 
-# Create your views here.
-@csrf_protect
-def create(request):
-    print(request.body)
-    return JsonResponse(request.body, content_type='application/json', safe=False)
+# Urmatorul form corespunde cu un dictionar  {"book": "un string"}
+class BookForm(forms.Form):
+    book = forms.CharField(max_length=100)
 
 
-# Read
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        """
+            What is parsing?
+            when the request from insomnia contains a form (multipart),
+            request.body will look like this:
+        
+                b'--X-INSOMNIA-BOUNDARY
+                Content-Disposition: form-data; name="book"
+                
+                Learn And Grow Rich
+                --X-INSOMNIA-BOUNDARY
+                Content-Disposition: form-data; name="genre"
+                
+                Motivation
+                --X-INSOMNIA-BOUNDARY--
+                '
+            
+            Parser translate encoded data, such as the form above,
+            into data we can use.
+            
+            E.g. By running 
+                
+            form = BookForm(request.POST)
+            print(form.data)
+            
+            form.data will contain:
+            
+            <QueryDict: {'book': ['Learn And Grow Rich'], 'genre': ['Motivation']}>
+            
+            * form.files will contain the files
+            
+            
+        """
+        print(request.body)
 
-def read_all(request):
-    return JsonResponse(json.dumps({"books": books}), content_type='application/json', safe=False)
+        # parse the form from the request
+        form = BookForm(request.POST)
+
+        book = form.data.get('book')
+        books.append(book)
+
+        return JsonResponse({"books": books}, content_type='application/json')
+    elif request.method == 'GET':
+        return JsonResponse({"books": books})
 
 
-def read(request, index):
-    return JsonResponse(json.dumps({"book": books[index]}), content_type='application/json', safe=False)
+@csrf_exempt
+def one(request, index: int):
+    # Treat the errors:
+    if index >= len(books) or index < 0:
+        return HttpResponse(f"Book Of Index={index} Not Found", status=404)
+    if request.method == 'POST':
+        return HttpResponse('POST method not permitted', status=405)
+
+    if request.method == 'GET':
+        return JsonResponse({"book": books[index]})
+
+    if request.method == 'DELETE':
+        del books[index]
+        return JsonResponse({"books": books})
+
+    # TODO for replacing and updating books, there are more methods (PUT, PATCH)
+    #      but for using them we'll need to use Django REST Framework
